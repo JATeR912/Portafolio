072ed45a66da15c03ced737f3070505e6b83f311
JATeR912 <jater.code@gmail.com>
2025-11-25 00:08:17 -0300

    Ajuste error 403 y 404

--- portafolio/urls.py
+++ portafolio/urls.py
@@ -18,11 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler403, handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('proyectos.urls')),
 ]
+
+handler403 = 'proyectos.views.error_403'
+handler404 = 'proyectos.views.error_404'
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
